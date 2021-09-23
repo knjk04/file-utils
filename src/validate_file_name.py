e2c3fb02c2e6b2613acc30db44dbb5617a908358
Karan <karan.n.j.kumar@gmail.com>
2021-09-23 19:18:33 +0100

    Write Windows file name regex validator

--- src/validate_file_name.py
+++ src/validate_file_name.py
@@ -1,12 +1,13 @@
 import re
 
-# Based on https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file
+# The rules of whether a file name is valid is based on https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file
 def is_valid_windows_file_name(file_name):
     # TODO add other checks
-    return not is_reserved_word(file_name)
-
+    return is_not_reserved_word(file_name) and not_contains_reserved_char(file_name) and \
+           not_ends_in_space_or_period(file_name)
 
 '''
+    The following reserved chars are not permitted at all:
     < (less than)
     > (greater than)
     : (colon)
@@ -17,20 +18,21 @@ def is_valid_windows_file_name(file_name):
     ? (question mark)
     * (asterisk)
 '''
-def contains_reserved_char():
-    raise NotImplementedError
+def not_contains_reserved_char(word):
+    # single backslash, vertical bar, question mark and asterisk are escaped
+    regex = re.compile(r'<+|>+|:+|"+|/+|\\+|\|+|\?+|\*+')
+    return regex.search(word) is None
 
 
-def is_reserved_word(word: str):
+def is_not_reserved_word(word: str):
     reserved_words = {
        'CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9', 'LPT1',
        'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9'
     }
-    return word in reserved_words
+    return word not in reserved_words
 
 
-def ends_in_space_or_period():
-    raise NotImplementedError
-
-
-
+def not_ends_in_space_or_period(word: str):
+    # TODO: fix. This matches when there's a space at the start and not a space or period at the end
+    regex = re.compile(r'(\S)*(\.)|( +)')
+    return regex.search(word) is None
