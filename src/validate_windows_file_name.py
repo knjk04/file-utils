d7296e0a77f251503be87608d54d853985b9dc33
Karan <karan.n.j.kumar@gmail.com>
2021-09-24 18:41:13 +0100

    Flip condition for contains reserved char for readability

--- src/validate_windows_file_name.py
+++ src/validate_windows_file_name.py
@@ -1,14 +1,24 @@
 import re
 
+
 # The rules of whether a file name is valid is based on
 # https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file
 def is_valid_windows_file_name(file_name):
-    return is_not_reserved_word(file_name) \
-           and not_contains_reserved_char(file_name) \
-           and not_ends_in_space_or_period(file_name)
+    if is_reserved_word(file_name):
+        raise Exception(f'You entered {file_name}, but you cannot use any of these reserved '
+                        f'words:  {get_reserved_words()}')
+    elif contains_reserved_char(file_name):
+        raise Exception(f'You entered {file_name}, but you cannot use any of these characters: '
+                        f'{get_reserved_char_list()}')
+
+    return not_ends_in_space_or_period(file_name)
 
 
-def not_contains_reserved_char(word: str):
+def get_reserved_char_list():
+    return ['<', '>', ':', '"', '/', '\\ (single backslash)', '|', '?', '*']
+
+
+def contains_reserved_char(word: str):
     """
         The following reserved chars are not permitted at all:
         < (less than)
@@ -22,16 +32,20 @@ def not_contains_reserved_char(word: str):
         * (asterisk)
     """
     # single backslash, vertical bar, question mark and asterisk are escaped
-    return re.match(r'\S*(<+|>+|:+|"+|/+|\\+|\|+|\?+|\*+)', word) is None
+    return re.match(r'\S*(<+|>+|:+|"+|/+|\\+|\|+|\?+|\*+)', word) is not None
 
 
-def is_not_reserved_word(word: str):
-    reserved_words = {
-       'CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5',
-       'COM6', 'COM7', 'COM8', 'COM9', 'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5',
-       'LPT6', 'LPT7', 'LPT8', 'LPT9'
+# This is its own function because we want to use it in multiple places while avoiding using a global
+def get_reserved_words():
+    return {
+        'CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5',
+        'COM6', 'COM7', 'COM8', 'COM9', 'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5',
+        'LPT6', 'LPT7', 'LPT8', 'LPT9'
     }
-    return word not in reserved_words
+
+
+def is_reserved_word(word: str):
+    return word in get_reserved_words()
 
 
 def not_ends_in_space_or_period(word: str):
--- tests/test_validate_windows_file_name.py
+++ tests/test_validate_windows_file_name.py
@@ -1,54 +1,54 @@
 import pytest
 
-from src.validate_windows_file_name import is_not_reserved_word, not_ends_in_space_or_period, \
-    not_contains_reserved_char
+from src.validate_windows_file_name import is_reserved_word, not_ends_in_space_or_period, \
+    contains_reserved_char
 
 
 @pytest.mark.parametrize("reserved_word", ['CONA', 'PRNA', 'AUXA', 'NULA', 'COM1A', 'COM2A',
                                            'COM3A', 'COM4A', 'COM5A', 'COM6A', 'COM7A', 'COM8A',
                                            'COM9A', 'LPT1A', 'LPT2A', 'LPT3A', 'LPT4A', 'LPT5A',
                                            'LPT6A', 'LPT7A', 'LPT8A', 'LPT9A'])
-def test_is_not_reserved_word_at_start(reserved_word):
-    assert is_not_reserved_word(reserved_word)
+def test_is_reserved_word_at_start(reserved_word):
+    assert not is_reserved_word(reserved_word)
 
 
 @pytest.mark.parametrize("reserved_word", ['CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3',
                                            'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9', 'LPT1',
                                            'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8',
                                            'LPT9'])
-def test_is_not_reserved_word(reserved_word):
-    assert is_not_reserved_word(reserved_word) is False
+def test_is_reserved_word(reserved_word):
+    assert is_reserved_word(reserved_word)
 
 
 @pytest.mark.parametrize("reserved_word", ['SCON', 'SPRN', 'SAUX', 'SNUL', 'SCOM1', 'SCOM2',
                                            'SCOM3', 'SCOM4', 'SCOM5', 'SCOM6', 'SCOM7', 'SCOM8',
                                            'SCOM9', 'SLPT1', 'SLPT2', 'SLPT3', 'SLPT4', 'SLPT5',
                                            'SLPT6', 'SLPT7', 'SLPT8', 'SLPT9'])
-def test_is_not_reserved_word_at_end(reserved_word):
-    assert is_not_reserved_word(reserved_word)
+def test_is_reserved_word_at_end(reserved_word):
+    assert not is_reserved_word(reserved_word)
 
 
 @pytest.mark.parametrize('word', ['<', '>', ':', '"', '/', '\\', '|', '?', '*'])
-def test_not_contains_reserved_char(word):
-    assert not_contains_reserved_char(word) is False
+def test_contains_reserved_char(word):
+    assert contains_reserved_char(word)
 
 
 @pytest.mark.parametrize('word', ['<file', '>file', ':file', '"file', '/file', '\\file', '|file',
                                   '?file', '*file'])
-def test_not_contains_reserved_char_when_reserved_char_at_start(word):
-    assert not_contains_reserved_char(word) is False
+def test_contains_reserved_char_when_reserved_char_at_start(word):
+    assert contains_reserved_char(word)
 
 
 @pytest.mark.parametrize('word', ['fil<e', 'fil>e', 'fil:e', 'fil"e', 'fil/e', 'fil\\e', 'fil|e',
                                   'fil?e', 'fil*e'])
-def test_not_contains_reserved_char_when_reserved_char_in_middle(word):
-    assert not_contains_reserved_char(word) is False
+def test_contains_reserved_char_when_reserved_char_in_middle(word):
+    assert contains_reserved_char(word)
 
 
 @pytest.mark.parametrize('word', ['file<', 'file>', 'file:', 'file"', 'file/', 'file\\', 'file|',
                                   'file?', 'file*'])
-def test_not_contains_reserved_char_when_reserved_char_at_end(word):
-    assert not_contains_reserved_char(word) is False
+def test_contains_reserved_char_when_reserved_char_at_end(word):
+    assert contains_reserved_char(word)
 
 
 @pytest.mark.parametrize('word', [' file', '.file'])
